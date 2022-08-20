c1b3bf195b85c81de5170666ef4f0d75d5f1b847
Kiram Newme <you@example.com>
2022-08-20 09:40:13 +0530

    first commit

---
+++ Max_and_min.py
@@ -0,0 +1,20 @@
+dict={'a':5,'b':10,'c':2,'d':7}
+val=dict.values()
+l=list(val)
+m=l[0]
+m1=l[0]
+i=0
+while i<len(l):
+    if l[i]>m:
+        m=l[i]
+    if l[i]<m1:
+        m1=l[i]
+    i+=1
+print("maximum value:",m)
+print("minimum value:",m1)
+
+
+# dict={'a':5,'b':10,'c':2,'d':7}
+# val=dict.values()
+# print(max(val))
+# print(min(val))
